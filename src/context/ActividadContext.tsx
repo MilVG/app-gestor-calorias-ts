3da45a08edffc011b13ba5b03df3eba39f6a3477
notlim_dev <elmotigre6@gmail.com>
2024-09-19 22:13:36 -0500

    012: Incorporación de Funciones para sus uso general.
    se Agregó elas funciones que calculan el consumo de calorias, ademàs se
    importo usememo y se agregó el typado de cada variable.

--- src/context/ActividadContext.tsx
+++ src/context/ActividadContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, useReducer } from "react";
+import { createContext, Dispatch, ReactNode, useMemo, useReducer } from "react";
 
 import { actividadReducer, initialState, ActividadState, ActividadAcciones } from "../reducers/actividad-Reducer";
 type ActividadProviderProps = {
@@ -7,18 +7,32 @@ type ActividadProviderProps = {
 
 type ActividadContextProps = {
   state: ActividadState,
-  dispatch: Dispatch<ActividadAcciones>
+  dispatch: Dispatch<ActividadAcciones>,
+  caloriasConsumidas: number,
+  caloriasQuemadas: number,
+  totalCalorias: number
 }
 export const ActividadContext = createContext<ActividadContextProps>(null!)
 
 export const ActividadProvider = ({ children }: ActividadProviderProps) => {
 
   const [state, dispatch] = useReducer(actividadReducer, initialState)
+
+  const caloriasConsumidas = useMemo(() => state.actividad.reduce((total, actv) => actv.categoriasel === 1 ? total +
+    actv.calorias : total, 0), [state.actividad])
+  const caloriasQuemadas = useMemo(() => state.actividad.reduce((total, actv) => actv.categoriasel === 2 ? total +
+    actv.calorias : total, 0), [state.actividad])
+
+  const totalCalorias = useMemo(() => caloriasConsumidas - caloriasQuemadas, [state.actividad])
+
   return (
     <ActividadContext.Provider
       value={{
         state,
-        dispatch
+        dispatch,
+        caloriasConsumidas,
+        caloriasQuemadas,
+        totalCalorias
       }}
     >
       {children}
