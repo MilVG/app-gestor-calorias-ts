f9d9e4ed44a7a1113e9aa8a3786631d78729e1f9
notlim_dev <elmotigre6@gmail.com>
2024-09-19 22:41:18 -0500

    014: Incorporación de Variables del Componente ActtividadesList
    Se trasladó las funciones al context para sus uso general.

--- src/context/ActividadContext.tsx
+++ src/context/ActividadContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, Dispatch, ReactNode, useMemo, useReducer } from "react";
 
 import { actividadReducer, initialState, ActividadState, ActividadAcciones } from "../reducers/actividad-Reducer";
+import { categorias } from "../data";
+import { Atv } from "../types";
 type ActividadProviderProps = {
   children: ReactNode
 }
@@ -10,7 +12,10 @@ type ActividadContextProps = {
   dispatch: Dispatch<ActividadAcciones>,
   caloriasConsumidas: number,
   caloriasQuemadas: number,
-  totalCalorias: number
+  totalCalorias: number,
+  nombreCategoria: (categoria: Atv["categoriasel"]) => string[],
+  actividadesComprobacion: boolean
+
 }
 export const ActividadContext = createContext<ActividadContextProps>(null!)
 
@@ -18,12 +23,18 @@ export const ActividadProvider = ({ children }: ActividadProviderProps) => {
 
   const [state, dispatch] = useReducer(actividadReducer, initialState)
 
+  //utilización de variables en actividadTracker
   const caloriasConsumidas = useMemo(() => state.actividad.reduce((total, actv) => actv.categoriasel === 1 ? total +
     actv.calorias : total, 0), [state.actividad])
   const caloriasQuemadas = useMemo(() => state.actividad.reduce((total, actv) => actv.categoriasel === 2 ? total +
     actv.calorias : total, 0), [state.actividad])
 
   const totalCalorias = useMemo(() => caloriasConsumidas - caloriasQuemadas, [state.actividad])
+
+  //utilización de funciones en ActividadesList
+  const nombreCategoria = useMemo(() => (categoria: Atv['categoriasel']) => categorias.map(cat => cat.id === categoria ? cat.name : ''), [state.actividad])
+  const actividadesComprobacion = useMemo(() => state.actividad.length === 0, [state.actividad])
+
 
   return (
     <ActividadContext.Provider
@@ -32,7 +43,9 @@ export const ActividadProvider = ({ children }: ActividadProviderProps) => {
         dispatch,
         caloriasConsumidas,
         caloriasQuemadas,
-        totalCalorias
+        totalCalorias,
+        nombreCategoria,
+        actividadesComprobacion
       }}
     >
       {children}
